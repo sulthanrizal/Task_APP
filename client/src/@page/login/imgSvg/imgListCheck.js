82027e99b0f15f456ebe384a2edc1c5a84e3bb5a
sulthanrizal <surie0312@gmail.com>
2024-02-28 06:48:52 +0700

    create function for img LOGIN

---
+++ client/src/@page/login/imgSvg/imgListCheck.js
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+
+const ImgListCheck = () => {
+    return (
+        <Box className="container-img-login">
+            <svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M19.9844 3.9845H10.9844V6.00012H19.9844V3.9845ZM19.9844 12.0001H10.9844V14.0157H19.9844V12.0001ZM3.53125 8.01575L0.015625 4.45325L1.42188 3.047L3.53125 5.15637L7.75 0.937622L9.20312 2.34387L3.53125 8.01575ZM3.53125 15.9845L0.015625 12.4689L1.42188 11.0626L3.53125 13.172L7.75 8.95325L9.20312 10.3595L3.53125 15.9845Z" fill="black" fill-opacity="0.8" />
+            </svg>
+
+        </Box>
+    )
+}
+export default ImgListCheck;
