f866f964ad0f8c3f322b9659c790e2b332749766
sulthanrizal <surie0312@gmail.com>
2024-02-28 06:26:59 +0700

    Img for componets page home

---
+++ client/src/@components/imgSvg/imgColorPurple.js
@@ -0,0 +1,21 @@
+import { Box } from "@chakra-ui/react";
+
+const ImgColorPurple = () => {
+    return (
+        <Box className="img-color-purple">
+            <svg width="411" height="717" viewBox="0 0 411 717" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g filter="url(#filter0_f_69_183)">
+                    <path d="M-130.165 114.186C-95.3682 38.6046 138.751 -21.3706 195.56 4.78364C252.369 30.9379 336.912 153.023 302.115 228.604C267.319 304.185 188.766 591.622 131.957 565.468C-23.4705 794.459 -164.962 189.767 -130.165 114.186Z" fill="#BDE0FE" />
+                </g>
+                <defs>
+                    <filter id="filter0_f_69_183" x="-235.484" y="-101.446" width="645.74" height="818.395" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                        <feGaussianBlur stdDeviation="50" result="effect1_foregroundBlur_69_183" />
+                    </filter>
+                </defs>
+            </svg>
+        </Box>
+    )
+}
+export default ImgColorPurple;
