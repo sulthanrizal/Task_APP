e025c0e4c0cd440396b6fea2f02261f5f7c4cb31
sulthanrizal <surie0312@gmail.com>
2024-03-01 20:59:32 +0700

    create page TODO BOX

--- client/src/@page/todoBox/index.js
+++ client/src/@page/todoBox/index.js
@@ -1,16 +1,20 @@
 import CalendarBar from "@page/calendarBar";
 import TodosList from "@page/todosList";
+import moment from "moment";
+import { useState } from "react";
+import './todobox.scss'
 
 const { Box } = require("@chakra-ui/react")
 
 const TodoBox = () => {
+    const [selectedDate, setSelectedDate] = useState(moment(new Date()).format('YYYY-MM-DD'))
     return (
         <Box className="container-todobox">
             <Box className="header-todobox">
-                <CalendarBar />
+                <CalendarBar setSelectedDate={setSelectedDate} selectedDate={selectedDate} />
             </Box>
             <Box className="body-todobox">
-                <TodosList />
+                <TodosList setSelectedDate={setSelectedDate} selectedDate={selectedDate} />
             </Box>
         </Box>
     )
