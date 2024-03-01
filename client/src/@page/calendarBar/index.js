e8509ecd3b3842fc17552f62bd41a54517769b09
sulthanrizal <surie0312@gmail.com>
2024-03-01 20:56:57 +0700

    create components CALENDAR BAR

--- client/src/@page/calendarBar/index.js
+++ client/src/@page/calendarBar/index.js
@@ -4,20 +4,19 @@ import { useEffect, useState } from "react";
 import moment from "moment";
 
 
-const CalendarBar = () => {
+const CalendarBar = ({ selectedDate, setSelectedDate }) => {
 
     const [date, setDate] = useState([])
     const today = new Date();
     const listDay = () => {
         const array = []
-        const firstDate = moment(today).format("YYYY-M-D")
-        const endDate = moment(today).add('day', 7).format("YYYY-M-D");
-        for (let TotalDay = firstDate; TotalDay < endDate; TotalDay = moment(TotalDay).add('day', 1).format('YYYY-M-D')) {
+        const firstDate = moment(today).format("YYYY-MM-DD")
+        const endDate = moment(today).add('day', 7).format("YYYY-MM-DD");
+        for (let TotalDay = firstDate; TotalDay < endDate; TotalDay = moment(TotalDay).add('day', 1).format('YYYY-MM-DD')) {
             array.push(TotalDay)
             setDate(array)
         }
     }
-    console.log(date, ' data')
 
     useEffect(() => {
         listDay()
@@ -29,18 +28,20 @@ const CalendarBar = () => {
             <Box className="calendar-title">Today</Box>
             <Flex className="container-date">
                 {
-                    date.map((tanggal,index) => {
+                    date.map((tanggal, index) => {
                         return (
-                            <Box className="list-date" key={index}>
-                                <Box className="list-date-day">
-                                    <Text>
-                                     {moment(tanggal).format('ddd')}
-                                    </Text>
-                                </Box>
-                                <Box className="list-date-tanggal">
-                                    <Text>
-                                     {moment(tanggal).format('D')}
-                                    </Text>
+                            <Box className="list-date" key={index} style={{ background: tanggal === selectedDate ? "#BA68C8" : "#DEB5E4" }}>
+                                <Box onClick={() => { setSelectedDate(tanggal) }}>
+                                    <Box className="list-date-day" >
+                                        <Text>
+                                            {moment(tanggal).format('ddd')}
+                                        </Text>
+                                    </Box>
+                                    <Box className="list-date-tanggal">
+                                        <Text>
+                                            {moment(tanggal).format('D')}
+                                        </Text>
+                                    </Box>
                                 </Box>
                             </Box>
                         )
