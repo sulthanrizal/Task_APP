0c3a2a3bf4ab4f437e49fad1b83b626e08041635
sulthanrizal <surie0312@gmail.com>
2024-03-01 21:19:31 +0700

    AXIOS create to take data from backend

--- client/src/@service/axios.js
+++ client/src/@service/axios.js
@@ -2,9 +2,10 @@ import Axios from "axios";
 
 const AxiosInstance = Axios.create({
     baseURL: 'https://crudapi.co.uk/api/v1',
-   headers: {
+    headers: {
         Authorization: "Bearer vg_-7Pc_SHyxPqTkiHsSoWnFVuUlVYI7K6RdovVOXB3dylIVkA"
-    } 
+    }
+
 });
 
 export default AxiosInstance
