8c4ba306748e92155325e659761f1fdd2290b57c
sulthanrizal <surie0312@gmail.com>
2024-03-05 16:34:21 +0700

    create components TODO ITEM

--- client/src/@page/todoItem/index.js
+++ client/src/@page/todoItem/index.js
@@ -4,18 +4,26 @@ import { deleteTodos, readTodos } from '@page/helpers/crud'
 import TodoAdd from '@page/todoadd'
 import { onClickNav } from '@components/helperOnClick'
 import { useNavigate } from 'react-router-dom'
+import router from '@router'
 const { Box, Flex } = require("@chakra-ui/react")
 
 
 const TodoItem = ({ todo, setTodos }) => {
     const navigate = useNavigate()
+    const id = todo._uuid
+
+    // const BtnEdit = () => {
+    //     onClickNav({ path: '/todoadd', navigate })
+    //     TodoAdd({ edit, setEdit })
+    // }
 
     return (
-        <Flex className="container-todoitem"
-            style={{ backgroundColor: todo.backgroundColor }}
-            onClick={() => { onClickNav({ path: '/todoadd', navigate }) }}
-        >
-            <Box className="flex-todoitem">
+        <Flex className="container-todoitem" style={{ backgroundColor: todo.backgroundColor }}  >
+            <Box className="flex-todoitem"
+                onClick={() => {
+                    onClickNav({ path: `/todoedit/:${id}`, navigate });
+                }}
+            >
                 {todo.title}
             </Box>
             <Box className="row-todoitem">
